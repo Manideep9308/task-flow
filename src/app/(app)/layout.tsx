4340a65d9293c4f6f38c9813e0dd82e0e3ff57a4
Mylapuram Manideep <mylapurammanideep@gmail.com>
2025-05-18 14:19:10 +0000

    you are giving an empty space in dashboard and other feature at top remove it

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -48,7 +48,7 @@ export default function AppLayout({
       <SidebarProvider defaultOpen={true}>
         <div className="flex min-h-screen w-full flex-col bg-muted/40">
           <AppSidebar />
-          <div className="flex flex-col sm:gap-4 sm:py-4 print:pl-0 md:peer-data-[state=expanded]:pl-[var(--sidebar-width)] md:peer-data-[state=collapsed]:pl-[var(--sidebar-width-icon)] transition-[padding-left] duration-200 ease-linear">
+          <div className="flex flex-col sm:gap-4 print:pl-0 md:peer-data-[state=expanded]:pl-[var(--sidebar-width)] md:peer-data-[state=collapsed]:pl-[var(--sidebar-width-icon)] transition-[padding-left] duration-200 ease-linear">
             <AppHeader />
             <main className="flex-1 p-4 sm:px-6 sm:py-0 md:gap-8">
               {children}
